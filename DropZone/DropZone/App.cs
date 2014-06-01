894131dbd1c690abe226d1d93369ecb93b9481d4
Chris Pilcher <nzchrisp@gmail.com>
2014-06-01 23:31:27 +1200

    Added main page which links to add jump page.

--- DropZone/DropZone/App.cs
+++ DropZone/DropZone/App.cs
@@ -15,11 +15,20 @@ namespace DropZone
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
         public static Page GetMainPage()
         {
+            return new NavigationPage(new Main());
+        }
+
+        /// <summary>
+        /// Gets the log entry page.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
+        public static Page GetLogEntryPage()
+        {
             LogEntry logEntry = new LogEntry
             {
                 BindingContext = new LogEntryViewModel()
             };
-            return new NavigationPage(logEntry);
+            return logEntry;
         }
     }
 }
---
+++ DropZone/DropZone/Views/Main.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DropZone.Views
+{
+    /// <summary>
+    /// The main page of the Drop Zone application.
+    /// </summary>
+    public partial class Main : ContentPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Main"/> class.
+        /// </summary>
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Called when add entry button is clicked.
+        /// </summary>
+        public async void OnButtonClicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(App.GetLogEntryPage());
+        }
+    }
+}
