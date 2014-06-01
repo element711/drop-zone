aeaa040fcd946ae973d27118d31c290926b07f1a
Chris Pilcher <nzchrisp@gmail.com>
2014-06-01 15:07:26 +1200

    Project setup for android project.

--- DropZone/DropZone.Android/MainActivity.cs
+++ DropZone/DropZone.Android/MainActivity.cs
@@ -1,19 +1,19 @@
-﻿using System;
-
-using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using Android.App;
 using Android.OS;
 
 using Xamarin.Forms.Platform.Android;
 
 namespace DropZone.Droid
 {
+    /// <summary>
+    /// Main activity.
+    /// </summary>
     [Activity(Label = "DropZone", MainLauncher = true)]
     public class MainActivity : AndroidActivity
     {
+        /// <summary>
+        /// Called when the activity is created.
+        /// </summary>
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
